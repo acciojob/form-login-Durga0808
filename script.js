32422efcfb17480a7bcd1d21860f69ca2e648948
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2024-02-19 21:43:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function getFormvalue() {
     //Write your code here
+	var fullname="";
+	var form = document.getElementById("form1");
+	// Get the value of the first name input
+    var firstName = form.elements["fname"].value;
 
+    // Get the value of the last name input
+    var lastName = form.elements["lname"].value;
+
+    // Concatenate the first and last names with a space in between
+    fullName = firstName + " " + lastName;
+
+    // Display the full name using an alert
+    alert(fullName);
 }
